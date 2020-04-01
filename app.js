66229cc6a33f578e6c608b00cb60987d4771aa41
mayafinkman <59428672+mayafinkman@users.noreply.github.com>
2020-04-01 09:58:20 -0400

    new heroku push

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //Import express
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+//initiate express to app
+const app = express();
+// set the port - if env.PORT use that, otherwise use 4000
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Exercise Four'))
+app.get('/', (req, res) => res.send('Exercise Four'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`));
